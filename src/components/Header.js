18c01f92a433087b5b0d8eeb3f3fdcc1d46c1cf7
squarepancakes <goh.shuyun@gmail.com>
2019-11-17 16:05:57 +0800

    add function to conditionally render depending on loginStatus

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+// import Logo from "../assets/Logo.png";
 
 export const Header = ({ loginStatus }) => {
 	return (
 		<div data-testid={"navBar"} className="navBar">
-			<Link to="/">Home</Link>
-			<Link to="/findnewrecipes">Find New Recipes</Link>
-			<Link to="/cookbook">Cookbook</Link>
-			{console.log("head", loginStatus)}
-			{loginStatus ? (
-				<Link to="/logout">Logout</Link>
-			) : (
-				<Link to="/login">Login</Link>
-			)}
-			<Link to="/signup">Signup</Link>
+			{/* <Link to="/">Home</Link> */}
+			{/* <img src={Logo} alt="app-etizer" /> */}
+			<Link to="/">App-etizer!</Link>
+			{loginStatus ? <Link to="/cookbook">Cookbook</Link> : ""}
+			<div className="members">
+				{loginStatus ? (
+					<Link to="/logout">Logout</Link>
+				) : (
+					<Link to="/login">Login</Link>
+				)}
+				<Link to="/signup">Signup</Link>
+			</div>
 		</div>
 	);
 };
